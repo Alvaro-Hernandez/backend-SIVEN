d41ca7acd68ed65b6962bff852cea8f1bbd17e0e
Alvaro-Hernandez <alvaropineda606@gmail.com>
2024-09-08 05:49:34 +0200

    :tada: Proyecto Base API

---
+++ src/main/java/com/healthbytes/siven/api/siven_api/SivenApiApplication.java
@@ -0,0 +1,13 @@
+package com.healthbytes.siven.api.siven_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SivenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SivenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthbytes/siven/api/siven_api/SivenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthbytes.siven.api.siven_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SivenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
