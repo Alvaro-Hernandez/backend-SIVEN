6ef6c5176410d7e92b6be25eee4ba5c6a844d09b
Alvaro-Hernandez <alvaropineda606@gmail.com>
2024-09-19 23:25:44 +0200

    :lipstick: Agregar Sub Paquete de repositorio de Captacion

--- src/main/java/com/healthbytes/siven/api/siven_api/repositories/CatalogoCaptacionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.healthbytes.siven.api.siven_api.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import com.healthbytes.siven.api.siven_api.entities.EventoSalud;
-
-public interface CatalogoCaptacionRepository extends CrudRepository<EventoSalud, Integer> {
-
-}
