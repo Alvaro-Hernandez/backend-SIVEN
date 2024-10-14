ec6eea086165c49aac33578e101033f967f57ca9
Alvaro-Hernandez <alvaropineda606@gmail.com>
2024-10-14 01:45:14 -0600

    :lipstick: Añadir Configuraciones, Servicios y Utilidades para JWT

---
+++ src/main/java/com/healthbytes/siven/api/siven_api/security/SpringSecurityConfig.java
@@ -0,0 +1,86 @@
+package com.healthbytes.siven.api.siven_api.security;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+// import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity; 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.healthbytes.siven.api.siven_api.security.filter.JwtAuthenticationFilter;
+import com.healthbytes.siven.api.siven_api.security.filter.JwtValidationFilter;
+
+@Configuration
+// @EnableMethodSecurity(prePostEnabled = true)
+public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeHttpRequests(
+                (requests) -> requests
+                        .requestMatchers(HttpMethod.GET, "/v1/usuarios/list-users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/usuarios/register-user").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/usuarios/create-user").hasRole("ADMIN")
+                        .requestMatchers("/v1/catalogo/captacion/**").hasAnyRole("ADMIN", "EPIDEMIOLOGO")
+                        .requestMatchers("/v1/catalogo/red-servicio/**").hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated())
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtValidationFilter(authenticationManager()))
+                .csrf(config -> config.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .sessionManagement(management -> management.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    // Manejo de cors para los clientes
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+                new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
