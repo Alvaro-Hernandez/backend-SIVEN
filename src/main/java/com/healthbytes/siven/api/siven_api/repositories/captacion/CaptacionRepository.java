39cab379d9476c3dd8d55f9ca48e27693c00bf76
Alvaro-Hernandez <alvaropineda606@gmail.com>
2024-11-21 02:38:35 -0600

    :lipstick: Modificacion de nombre de ruta al nuevo Service SP

--- src/main/java/com/healthbytes/siven/api/siven_api/repositories/captacion/CaptacionRepository.java
+++ src/main/java/com/healthbytes/siven/api/siven_api/repositories/captacion/CaptacionRepository.java
@@ -3,8 +3,8 @@ package com.healthbytes.siven.api.siven_api.repositories.captacion;
 import org.springframework.data.repository.CrudRepository;
 
 import com.healthbytes.siven.api.siven_api.entities.Captacion;
-import com.healthbytes.siven.api.siven_api.services.CaptacionRepositoryCustom;
+import com.healthbytes.siven.api.siven_api.services.SPCaptacionService;
 
-public interface CaptacionRepository extends CrudRepository<Captacion, Integer>, CaptacionRepositoryCustom {
+public interface CaptacionRepository extends CrudRepository<Captacion, Integer>, SPCaptacionService {
 
 }
